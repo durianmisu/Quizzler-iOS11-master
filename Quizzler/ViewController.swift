479c36dfbd9be9f34275e02c4df026250e887ee9
Jonathan Lo <jonathanlo@jonathans-macbook-pro-2.local>
2018-12-28 23:24:25 -0800

    Added the rest of the questions and set up logics for checking answers and updating the score and progress

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,41 +11,90 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
+    var score : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIView!
     @IBOutlet weak var scoreLabel: UILabel!
-    @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        let firstQuestion = allQuestions.list[0]
+//        questionLabel.text = firstQuestion.questionText
+        
+            nextQuestion()
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        questionNumber += 1
+        nextQuestion()
+
     }
     
     
     func updateUI() {
+        
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "Question: \(questionNumber + 1) / 13"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
       
     }
     
 
     func nextQuestion() {
-        
+        if questionNumber <= 12 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+            
+            updateUI()
+            
+        } else {
+//            print("End of quiz")
+            let alert = UIAlertController(title: "You are done", message: "Do you want to start again?", preferredStyle: .alert)
+            
+            // when we see "in" we need to use the prefix "self." before calling the method
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
     }
     
     
     func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionNumber].answer
         
+        if correctAnswer == pickedAnswer {
+            print("You got it")
+            score += 1
+        } else {
+            print("Incorrect")
+            score -= 1
+        }
     }
     
     
     func startOver() {
-       
+        questionNumber = 0
+        score = 0
+        nextQuestion()
     }
     
 
